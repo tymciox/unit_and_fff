a122af3d4724aebb94e0130a40aaf944ed32a307
Tymoteusz Hurko <tymoteusz.hurko@andra.com.pl>
2022-12-26 15:56:14 +0100

    dodanie nagłówka pliku fff

--- test/TestProductionCode.c
+++ test/TestProductionCode.c
@@ -1,6 +1,12 @@
 #include "unity.h"
 #include "unity_fixture.h"
+#include "fff.h"
 #include "vbat.h"
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VOID_FUNC(IO_MEM_WR8, uint32_t, uint8_t);
+FAKE_VALUE_FUNC(uint8_t, IO_MEM_RD8, uint32_t);
 
 TEST_GROUP(ProductionCode);
 
