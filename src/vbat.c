2f8feee7d0fbf4cb9927ed713dcfb883c3ad3e10
Tymoteusz Hurko <tymoteusz.hurko@andra.com.pl>
2022-12-26 17:21:15 +0100

    green: test pass

--- src/vbat.c
+++ src/vbat.c
@@ -3,13 +3,32 @@
 #include "led_driver.h"
 
 static int alarm = 0;
+static int alarm_cnt = 0;
 
 void vbat_check(void)
 {
-    alarm = 1;
+    if (read_adc() < ALARM_THRESHOLD_LEVEL)
+    {
+        alarm_cnt++;
+    }
+    else
+    {
+        alarm_cnt = 0;
+    }
+    
+    if (alarm_cnt >= 5)
+    {
+        alarm = 1;
+    }
 }
 
 int vbat_check_alarm_state(void)
 {
     return alarm;
+}
+
+void vbat_init(void)
+{
+    alarm = 0;
+    alarm_cnt = 0;
 }
--- test/TestProductionCode.c
+++ test/TestProductionCode.c
@@ -5,14 +5,14 @@
 
 DEFINE_FFF_GLOBALS;
 
-FAKE_VALUE_FUNC(adc_raw_t, adc_read);
+FAKE_VALUE_FUNC(adc_raw_t, read_adc);
 FAKE_VOID_FUNC(IO_MEM_RD8, uint32_t);
 
 TEST_GROUP(ProductionCode);
 
 TEST_SETUP(ProductionCode)
 {
-
+  vbat_init();
 }
 
 TEST_TEAR_DOWN(ProductionCode)
@@ -22,7 +22,7 @@ TEST_TEAR_DOWN(ProductionCode)
 
 TEST(ProductionCode, FiveSignalBelowAlarm)
 {
-  adc_read_fake.return_val = ALARM_THRESHOLD_LEVEL-1;
+  read_adc_fake.return_val = ALARM_THRESHOLD_LEVEL-1;
 
   for (int i=0; i<5; i++)
   {
@@ -33,12 +33,12 @@ TEST(ProductionCode, FiveSignalBelowAlarm)
 
 TEST(ProductionCode, FourSignalBelowAlarm1OverTreshold)
 {
-  adc_read_fake.return_val = ALARM_THRESHOLD_LEVEL-1;
+  read_adc_fake.return_val = ALARM_THRESHOLD_LEVEL-1;
   for (int i=0; i<4; i++)
   {
     vbat_check();
   }
-  adc_read_fake.return_val = ALARM_THRESHOLD_LEVEL;
+  read_adc_fake.return_val = ALARM_THRESHOLD_LEVEL;
   TEST_ASSERT_EQUAL_INT(0, vbat_check_alarm_state());
 }
 
